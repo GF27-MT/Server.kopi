6a81c2bbf54ac5733d9c4b6ceb00224dde5bf996
GF27-MT <Gryx0673@euclive.dk>
2025-05-06 13:32:39 +0200

    nnn

--- server.js
+++ server.js
@@ -191,7 +191,7 @@ const Opskrift = mongoose.model('Opskrift', {
 // GET: Hent alle opskrifter
 app.get('/opskrifter', async (req, res) => {
   try {
-    const opskrifter = await Opskrift.find().limit(4168);
+    const opskrifter = await Opskrift.find().limit(1000);
     res.json(opskrifter);
   } catch (err) {
     res.status(500).json({ message: 'Fejl ved hentning af opskrifter' });
